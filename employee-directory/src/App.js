ceeb4e98c70baa4ca8f9ac6e5562b52fc9ac6621
DukeWilki <65857753+DukeWilki@users.noreply.github.com>
2021-01-11 20:43:13 +1100

    all 3 containers render - no data

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,49 +1,60 @@
 import logo from "./logo.svg";
 import "./App.css";
 import EmployeeListHero from "./containers/EmployeeListHero/EmployeeListHero";
+import EmployeeListSearch from "./containers/EmployeeListSearch/EmployeeListSearch";
+import EmployeeListTable from "./containers/EmployeeListTable/EmployeeListTable";
 
 function App() {
-  return <EmployeeListHero />;
-  // return (
-  //   <div className="App">
-      {/* <div class="jumbotron jumbotron-fluid">
-        <div class="container">
-          <h1 class="display-4">Employee Directory</h1>
-          <p class="lead">
-            This is a modified jumbotron that occupies the entire horizontal
-            space of its parent.
+  // return <EmployeeListHero /> ;
+
+  
+
+    return (
+      
+      <div className="App">
+        <EmployeeListHero />
+        <EmployeeListSearch />
+        <EmployeeListTable />
+        {/* <div class="jumbotron jumbotron-fluid">
+    <div class="container">
+      <h1 class="display-4">Employee Directory</h1>
+      <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+    </div>
+  </div>   */}
+
+  
+  {/* <table class="table">
+    <thead class="thead-dark">
+      <tr>
+        <th scope="col">Photo</th>
+        <th scope="col">Name</th>
+        <th scope="col">Phone </th>
+        <th scope="col">Email</th>
+        <th scope="col">DOB</th>
+      </tr>
+    </thead>
+    </table> */}
+
+    
+  
+  
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+                  <p>
+            Edit <code>src/App.js</code> and save to reload.
           </p>
-        </div>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn Reactgwerwergh
+          </a>
+        </header>
       </div>
-
-      <table class="table">
-        <thead class="thead-dark">
-          <tr>
-            <th scope="col">Photo</th>
-            <th scope="col">Name</th>
-            <th scope="col">Phone </th>
-            <th scope="col">Email</th>
-            <th scope="col">DOB</th>
-          </tr>
-        </thead>
-      </table> */}
-
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn Reactgwerwergh
-    //     </a>
-    //   </header>
-    // </div>
-  // );
+    );
+  
 }
 
 export default App;
--- employee-directory/src/containers/EmployeeListTable/EmployeeListTable.js
+++ employee-directory/src/containers/EmployeeListTable/EmployeeListTable.js
@@ -1,20 +1,22 @@
 import React from "react";
 import "./style.css";
+// import API from "../../utils/API.js";
 
-<table class="table">
-  <thead class="thead-dark">
-    <tr>
-      <th scope="col">Photo</th>
-      <th scope="col">Name</th>
-      <th scope="col">Phone </th>
-      <th scope="col">Email</th>
-      <th scope="col">DOB</th>
-    </tr>
-  </thead>
-
-
-
-
-
-  
-  </table>
+function EmployeeListTable() {
+  return (
+    <div className="container">
+      <table class="table">
+        <thead class="thead-dark">
+          <tr>
+            <th scope="col">Photo</th>
+            <th scope="col">Name</th>
+            <th scope="col">Phone </th>
+            <th scope="col">Email</th>
+            <th scope="col">DOB</th>
+          </tr>
+        </thead>
+      </table>
+    </div>
+  );
+}
+export default EmployeeListTable;
